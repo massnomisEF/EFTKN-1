6d98b3b297c91d59a98b267124149293be7e3605
massnomis <massnomis@gmail.com>
2022-03-20 15:50:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import plotly.express as px
 import json
 import time
 
+
+
+
+
+
+
 data = {
     'Name':
         [
@@ -27,7 +33,7 @@ data = {
          ]
 }
 df = pd.DataFrame(data)
-
+st.title("The EF LinkedIn Points Party")
 points = []
 for address in df['address']:
     bal_custom = requests.get(
@@ -40,17 +46,82 @@ df['points'] = points
 # print(df)
 
 df = df.sort_values(by=['points'], ascending=False)
+df['rank'] = df['points'].rank(ascending = False)
+st.write("The LeaderBoard")
+
+st.write(df)
+
 
 LeaderBoard = px.bar(
     df, #this is the dataframe you are trying to plot
     x = "Name",
     y = "points",
+    color = "rank",
     orientation = "v",
     template = "plotly_white",
     width = 1000,
     height = 600
 )
+st.write("The chart")
 st.plotly_chart(LeaderBoard)
 
-df['rank'] = df['points'].rank(ascending = False)
+
+st.write("The Recents")
+# recents =
+query_id = "28ec29cc-7bc8-4245-bd56-837ad4bedb15"
+df = pd.read_json(f"https://api.flipsidecrypto.com/api/v2/queries/{query_id}/data/latest",
+convert_dates=["TIMESTAMP_NTZ"],
+)
+df = px.bar(
+    df,  # this is the dataframe you are trying to plot
+    x="TIME",
+    y="ACTUAL_AMT",
+    color = "NAME",
+    orientation="v",
+    template="plotly_white",
+    width=1000,
+    height=600
+)
+st.plotly_chart(df)
+
+
+query_id = "b89af3ae-02e3-492e-868d-d2a2c9f669db"
+df = pd.read_json(f"https://api.flipsidecrypto.com/api/v2/queries/{query_id}/data/latest",
+convert_dates=["TIMESTAMP_NTZ"],
+)
 st.write(df)
+
+
+st.write("For the most recent transaction data, check on chain")
+st.markdown("""https://polygonscan.com/token/0x23a32ba2b63051950e8421c18fb300870993d9a9""")
+
+
+
+
+st.title("ahem...")
+
+st.markdown("""
+If you need a reminder - These are the participating positions:""")
+
+st.markdown(""" 
+R&D-
+DeFi Developer - efficientfrontier.28.324@applynow.io
+""")
+st.markdown(""" 
+
+Full-Stack / BackEnd - efficientfrontier.48.829@applynow.io """)
+st.markdown(""" 
+
+Head of DevOps - efficientfrontier.AA.82F@applynow.io """)
+st.markdown(""" 
+
+Head of QA and Automation - efficientfrontier.87.826@applynow.io""")
+st.markdown("""Product Manager - efficientfrontier.D5.32B@applynow.io""")
+st.markdown("""All R&D CVs you’re not sure where to send - efficientfrontier.92.C22@applynow.io""")
+
+st.markdown("""Trading: Trading Operations Specialist - efficientfrontier.9E.225@applynow.io""")
+
+st.markdown("""PLEASE don’t forget to add a link to our career pages in your posts! the link is efrontier.io/careers""")
+
+st.title("...")
+st.write("@massnomis")

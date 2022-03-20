7256f97a2083cbd6150d3e99a713ab30f1a8c807
massnomis <massnomis@gmail.com>
2022-03-20 15:10:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 import pandas as pd
 import streamlit as st
 import requests
+import plotly
 import plotly.express as px
+import json
+import time
 
 data = {
     'Name':
-        ['Yaniv', 'Avishay', 'Yasha', 'Sam'],
+        [
+         'Yaniv',
+         'Avishay',
+         'Yasha',
+         'Sam',
+         'Tim',
+         'Asif'
+         ],
     'address':
-        ["0x3e35698c962BCFfBab99594aff3fa136db78b300", "0xEe3dBd4d25000C599bBF3B4497D12c733e7A0cB4", "0x52B9C25c4d16a2efe83c89bcfC6fE259B05658A5", "0xba2ef5189B762bd4C9E7f0b50fBBaB65193935e8"]
+        [
+         "0x3e35698c962BCFfBab99594aff3fa136db78b300",
+         "0xEe3dBd4d25000C599bBF3B4497D12c733e7A0cB4",
+         "0x52B9C25c4d16a2efe83c89bcfC6fE259B05658A5",
+         "0xba2ef5189B762bd4C9E7f0b50fBBaB65193935e8",
+         "0x48E92AaDd3b92Aa875fe08BF27fe44080eEC749a",
+         "0x6bA98440b46d12b46d3393b21319c38AaC12249a"
+         ]
 }
 df = pd.DataFrame(data)
 
@@ -19,6 +36,7 @@ for address in df['address']:
     list.append(points, bal)
 
 df['points'] = points
+# print(df)
 
 df = df.sort_values(by=['points'], ascending=False)
 
@@ -32,3 +50,4 @@ LeaderBoard = px.bar(
     height = 600
 )
 st.plotly_chart(LeaderBoard)
+

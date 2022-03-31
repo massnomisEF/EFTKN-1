c3c29b7d883562dd206f42ec1e19b64f5e70008c
massnomisEF <98458206+massnomisEF@users.noreply.github.com>
2022-03-31 11:40:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ data = {
          'Ylann',
          'Roman',
          'Marine',
-         'Raz'
+         'Raz',
+         'Yuval Z'  
          ],
     'address':
         [
@@ -32,7 +33,8 @@ data = {
          "0xc562e686cfdc46a4cc7294447611ee8a952e4a03",
          "0xce04b56115e76918a998e1bdf989d56ff329b252",
          "0xb9d88621e03c4aceaf6572f0aef720a8fe37f03b",
-         "0xa68166badfc75a15ee2f8a30a8230f8e1b8b4f82"
+         "0xa68166badfc75a15ee2f8a30a8230f8e1b8b4f82",
+         "0x23A32Ba2B63051950e8421c18Fb300870993D9a9"   
          ]
 }
 df = pd.DataFrame(data)
